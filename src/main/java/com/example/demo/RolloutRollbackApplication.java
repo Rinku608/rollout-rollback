0a7d72af94424be4c0b63613a7bdd88700e18fde
Rinku608 <rinku.kumari@mavenwave.com>
2022-12-11 15:33:05 +0530

    new commit

---
+++ src/main/java/com/example/demo/RolloutRollbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolloutRollbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolloutRollbackApplication.class, args);
+	}
+
+}
